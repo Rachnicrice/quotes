ee3bd1515b8e352fdf808f81378515eee18bc3c0
Rachael Rice <Rachnicrice@gmail.com>
2020-01-19 13:58:19 -0800

    Added test for book constructor

--- src/test/java/quotes/BookTest.java
+++ src/test/java/quotes/BookTest.java
@@ -1,4 +1,19 @@
 package quotes;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class BookTest {
+    Book testBook;
+
+    @Before
+    public void setUp () {
+        testBook = new Book ("src/main/resources/recentquotes.json");
+    }
+
+    @Test
+    public void testBookConstructor () {
+        Assert.assertEquals("src/main/resources/recentquotes.json", testBook.bookPath);
+    }
 }
